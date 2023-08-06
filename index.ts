0af91eb81f8dc711ab02dfb33d766343c93078db
realebx <141572120+realebx@users.noreply.github.com>
2023-08-06 21:14:24 +0300

    asd

--- index.ts
+++ index.ts
@@ -1,28 +1,29 @@
 /* eslint-disable prettier/prettier */
 import { Vector2, Input, VMouseKeys, EventsSDK, RendererSDK, Color } from "github.com/octarine-public/wrapper/index"
 
-const InBounds = ( source: Vector2, start: Vector2, end: Vector2 ) => {
-	return source.x >= start.x && source.x <= end.x && source.y >= start.y && source.y <= end.y;
+const InBounds = ( source: Vector2, position: Vector2, size: Vector2 ) => {
+	return source.x >= position.x && source.x <= position.x + size.x && source.y >= position.y && source.y <= position.y + size.y;
 }
 
 class Draggable {
 	startPosition: Vector2;
-	endPosition: Vector2;
+	size: Vector2;
 
 	isDragging: boolean = false;
 
-	constructor( startPosition: Vector2, endPosition: Vector2 ) {
+	constructor( startPosition: Vector2, size: Vector2 ) {
 		this.startPosition = startPosition;
-		this.endPosition = endPosition;
+		this.size = size;
 	}
 
 	Update( ): void {
 		if ( !Input.IsMouseKeyDown( VMouseKeys.MK_LBUTTON ) )
 			this.isDragging = false;
-		
 	}
 }
 
 EventsSDK.on( "Draw", ( ) => {
-	RendererSDK.FilledRect( new Vector2( 200, 200 ), new Vector2( 100, 100 ), new Color( 255,255,255,255 ) )
+	const mousePos: Vector2 = Input.CursorOnScreen;
+	const color = InBounds( mousePos, new Vector2( 200, 200 ), new Vector2( 100, 100 ) ) ? new Color( 0, 255, 0, 255 ) : new Color( 255, 255, 255, 255 )
+	RendererSDK.FilledRect( new Vector2( 200, 200 ), new Vector2( 100, 100 ), color )
 } )
