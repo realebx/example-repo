dd9572b18f5369d3c2435638bf0a9d101faf5feb
realebx <141572120+realebx@users.noreply.github.com>
2023-08-06 21:07:34 +0300

    test

--- index.ts
+++ index.ts
@@ -1,5 +1,28 @@
-import { EventsSDK } from "github.com/octarine-public/wrapper/index"
+/* eslint-disable prettier/prettier */
+import { Vector2, Input, VMouseKeys, EventsSDK, RendererSDK, Color } from "github.com/octarine-public/wrapper/index"
 
-EventsSDK.on("GameStarted", () => {
-	console.log("Hello world!")
-})
+const InBounds = ( source: Vector2, start: Vector2, end: Vector2 ) => {
+	return source.x >= start.x && source.x <= end.x && source.y >= start.y && source.y <= end.y;
+}
+
+class Draggable {
+	startPosition: Vector2;
+	endPosition: Vector2;
+
+	isDragging: boolean = false;
+
+	constructor( startPosition: Vector2, endPosition: Vector2 ) {
+		this.startPosition = startPosition;
+		this.endPosition = endPosition;
+	}
+
+	Update( ): void {
+		if ( !Input.IsMouseKeyDown( VMouseKeys.MK_LBUTTON ) )
+			this.isDragging = false;
+		
+	}
+}
+
+EventsSDK.on( "Draw", ( ) => {
+	RendererSDK.FilledRect( new Vector2( 200, 200 ), new Vector2( 100, 100 ), new Color( 255,255,255,255 ) )
+} )
